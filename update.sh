75eb597e361d9a320bfcd1f4fb5cb31db7f20b2e
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2019-05-14 21:57:18 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,5 +4,12 @@ quantisnet-cli stop
 killall -9 quantisnetd
 cd /usr/local/bin
 rm quantisnetd quantisnet-cli quantisnet-tx test_quantisnet quantisnet-qt
-read -p "Enter URL for new wallet .tar.gz:" url
-wget url
+while true; do
+  read -p "Enter URL for new wallet .tar.gz:" url
+  case %url in
+    [http]* ) wget url; break;;
+    * ) echo "Please enter a valid URL.";;
+  esac
+done
+
+
