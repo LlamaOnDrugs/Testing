5542d3807e1d2fb508f776f4e489089fd3aa7b76
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2019-05-14 22:15:08 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@ cd ~
 quantisnet-cli stop
 killall -9 quantisnetd
 cd /usr/local/bin
-url = https://github.com/LlamaOnDrugs/Quan/blob/master/quan-mn-update.sh
-filename = $(basename "$url")
-wget = "$url"
+
+read -p "Enter URL : " VAR
+
+filename = $(basename "$VAR")
+wget = "$VAR"
