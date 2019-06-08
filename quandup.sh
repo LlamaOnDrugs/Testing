4b9c0be88022490b1cb93a5580bba4f706481536
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2019-06-08 15:14:35 +0100

    Create quandup.sh

---
+++ quandup.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+clear
+read -p "How many nodes to install?" VAR
+ipreq="$VAR"
+echo "Current list of IP addresses"
+dupmn iplist
+
+read -p "Enter starting IPV6 Address: " IPVAR
+
+iptrunc=${IPVAR%:*}":"
+
+counter=1
+while [ $counter -le $ipreq ]
+do
+  dupmn install quantisnet -bootstrap -ip="$iptrunc$counter"
+  ((counter++))
+done
+
+echo ALL done
