696fd4b98e16d01b6630ab3acdd16d4a08c98659
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2019-06-12 00:01:18 +0100

    Update ipadd.sh

--- ipadd.sh
+++ ipadd.sh
@@ -2,25 +2,17 @@
 clear
 read -p "How many IPV6 IP Addresses do you need?" VAR
 
-ipreq = "$VAR"
+ipreq="$VAR"
 
 echo "Current list of IP addresses"
 dupmn iplist
 
 read -p "Enter starting IPV6 Address: " IPVAR
 
-iptrunc = ${IPVAR%:*}
-ipstart = ${IPVAR#:*}
-
-var2=${var%_*}
-
-echo "$iptrunc\n"
-echo "$ipstart\n"
-
-counter = 1
+counter=1
 while [ $counter -le $ipreq ]
 do
-  dupmn ipadd "$ipstart" 64 eth0
+  dupmn ipadd "$IPVAR$counter" 64 eth0
   ((counter++))
 done
 
