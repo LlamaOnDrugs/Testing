e33b604e6bd776bcd6024d9667249af260dcf3cd
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2019-06-11 01:25:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ ipreq=380
 while [ $counter -le $ipreq ]
 do
   quantisnetd -daemon -conf=/root/.quantisnetcore"$counter"/quantisnet.conf -datadir=/root/.quantisnetcore"$counter"
-  dupmn ipadd "$ipstart" 64 eth0
   ((counter++))
 done
 
