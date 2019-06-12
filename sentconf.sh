9220ed1617930b3c445fa12e91c8ef1082d908f8
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2019-06-12 02:22:57 +0100

    Create sentconf.sh

---
+++ sentconf.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+clear
+
+read -p "How many installs?" VAR
+
+installs="$VAR"
+
+function conf_set_value() {
+	# <$1 = conf_file> | <$2 = key> | <$3 = value> | [$4 = force_create]
+	#[[ $(grep -ws "^$2" "$1" | cut -d "=" -f1) == "$2" ]] && sed -i "/^$2=/s/=.*/=$3/" "$1" || ([[ "$4" == "1" ]] && echo -e "$2=$3" >> $1)
+	local key_line=$(grep -ws "^$2" "$1")
+	[[ "$(echo $key_line | cut -d '=' -f1)" =~ "$2" ]] && sed -i "/^$2/c $(echo $key_line | grep -oP '^[\s\S]{0,}=[\s]{0,}')$3" $1 || $([[ "$4" == "1" ]] && echo -e "$2=$3" >> $1)
+}
+function conf_get_value() {
+	# <$1 = conf_file> | <$2 = key> | [$3 = limit]
+	[[ "$3" == "0" ]] && grep -ws "^$2" "$1" | cut -d "=" -f2 || grep -ws "^$2" "$1" | cut -d "=" -f2 | head $([[ ! $3 ]] && echo "-1" || echo "-$3")
+}
+
+
+counter=1
+while [ $counter -le $installs ]
+do
+  $(conf_set_value "/root/.quantisnetcore$counter/sentinel/sentinel.conf" "quantisnet_conf"		"/root/.quantisnetcore$counter/quantisnet.conf" 1)
+  ((counter++))
+done
